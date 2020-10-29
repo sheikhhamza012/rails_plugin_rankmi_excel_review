a3a84e08d93fd063a6fc90da2148a1c94fdf8834
sheikh hamza <sheikhhamza012@gmail.com>
2020-10-29 23:10:36 +0500

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
-RankmiExcelReview::Engine.routes.draw do
-    resources :hamza
+Rails.application.routes.draw do
+    scope module: 'rankmi_excel_review' do
+        resources :hamza
+    
+    end
+
 end
