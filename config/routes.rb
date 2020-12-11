539322ef8d24b488b3888b63927f3c881a1ef2a3
sheikh hamza <sheikhhamza012@gmail.com>
2020-12-11 14:48:20 +0500

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 RankmiExcelReview::Engine.routes.draw do
-    resources :review , only:[:index, :create]
+    resources :review , only:[:index, :create] do 
+        collection do
+            post :index
+        end
+    end
     resources :test, only:[:index]
 end
